10c9b0bc0edfb719237acd7be00e7745f25a1af8
Martin Dobiasch <martin.dobiasch@donau-uni.ac.at>
2025-01-23 15:46:35 +0100

    init and added sintering classes from yaml schema

---
+++ src/foobar/schema_packages/__init__.py
@@ -0,0 +1,30 @@
+from nomad.config.models.plugins import SchemaPackageEntryPoint
+from pydantic import Field
+
+
+class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from foobar.schema_packages.schema_package import m_package
+
+        return m_package
+
+
+schema_package_entry_point = NewSchemaPackageEntryPoint(
+    name='NewSchemaPackage',
+    description='New schema package entry point configuration.',
+)
+
+class SinteringEntryPoint(SchemaPackageEntryPoint):
+
+    def load(self):
+        from foobar.schema_packages.sintering import m_package
+
+        return m_package
+
+
+sintering = SinteringEntryPoint(
+    name='Sintering',
+    description='Schema package for describing a sintering process.',
+)
